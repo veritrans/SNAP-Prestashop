c85cd68cba0d018510670bc6261b5d4a3ad40200
Alexander Kevin <alexander.kevin@midtrans.com>
2018-07-27 14:33:34 +0700

    update version number

--- midtranspay.php
+++ midtranspay.php
@@ -72,7 +72,7 @@ class MidtransPay extends PaymentModule
 	{
 		$this->name = 'midtranspay';
 		$this->tab = 'payments_gateways';
-		$this->version = '2.3';
+		$this->version = '2.4';
 		$this->author = 'Midtrans';
 		$this->bootstrap = true;
 		
