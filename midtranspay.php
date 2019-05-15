b545011e83c2e76a1856d0cadbf264d2261d25ce
Rizda Dwi Prasetya <rizda.prasetya@midtrans.com>
2019-05-15 17:00:11 +0700

    version bump

--- midtranspay.php
+++ midtranspay.php
@@ -72,7 +72,7 @@ class MidtransPay extends PaymentModule
 	{
 		$this->name = 'midtranspay';
 		$this->tab = 'payments_gateways';
-		$this->version = '2.7.1';
+		$this->version = '2.7.2';
 		$this->author = 'Midtrans';
 		$this->bootstrap = true;
 		
